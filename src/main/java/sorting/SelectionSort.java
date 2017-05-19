c5d16abd6630ff1f197c6f48657036c1f064bd5b
mloszek <miloszloszek@gmail.com>
2017-05-19 20:58:15 +0200

    working selection sort;
    make class Person with arguments age and last name, make sorting of age,then last name

--- src/main/java/sorting/SelectionSort.java
+++ src/main/java/sorting/SelectionSort.java
@@ -8,14 +8,13 @@ public class SelectionSort {
         int[] tab = {1, -10, 5, 2, -666, -23, 6};
 
         for (int i = 0; i < tab.length; i++) {
-            for (int j = 1; j < tab.length; j++) {
-                if (tab [i] > tab [j]){
-                    int temp = tab [i];
-                    tab[i] = tab [j];
-                    tab [j] = temp;
+            for (int j = 1 + i; j < tab.length; j++) {
+                if (tab[i] > tab[j]) {
+                    int temp = tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = temp;
                 }
             }
-
         }
         for (int i = 0; i < tab.length; i++) {
             System.out.println(tab[i]);
